8bffdd50413196edf32a829fe3b107f0c48f1abe
mdnriyas17 <mdnriyas@gmail.com>
2023-04-28 07:50:02 +0530

    card

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./App.css";
+
+import Cart from "./pages/Cart";
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <section className="pricing py-5">
+   
+        <Cart/>
+      </section>
+    </div>
+  );
+}
+
+export default App;
